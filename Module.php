daed34f27026b868b7bbeca5f2db2dbebc0a6e16
Jorge Gonzalez <ljorgelgonzalez@gmail.com>
2017-09-24 17:42:01 -0400

    test module

--- Module.php
+++ Module.php
@@ -1,23 +1,82 @@
 <?php
 namespace lawiet\rbac;
 
+use yii\base\Module as BaseModule;
+use yii\filters\AccessControl;
+
 /**
- * lawiet module definition class
+ * module for rbac.
+ *
+ * @author Jorge Gonzalez
+ * @email ljorgelgonzalez@outlook.com
+ *
+ * @since 1.0
  */
-class Module extends \yii\base\Module
+
+class Module extends BaseModule
 {
-    /**
-     * @inheritdoc
-     */
-    public $controllerNamespace = 'app\lawiet\rbac\controllers';
+    /** 
+     * @inheritdoc 
+     */ 
+    public $controllerNamespace = 'lawiet\rbac\controllers'; 
 
     /**
-     * @inheritdoc
+     * @var string default route
      */
-    public function init()
+    public $defaultRoute = 'role/index';
+
+    /**
+     * @var array
+     */
+    public $admins = [];
+
+	/**
+     * @var string The Administrator permission name.
+     */
+    public $adminPermission;
+
+    /** @inheritdoc */
+    public function behaviors()
     {
-        parent::init();
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow'         => true,
+                        'roles'         => ['@'],
+                        'matchCallback' => [$this, 'checkAccess'],
+                    ]
+                ],
+            ],
+        ];
+    }
 
-        // custom initialization code goes here
+    /** 
+     * @inheritdoc 
+     */ 
+    public function init() 
+    { 
+        parent::init(); 
+
+        // custom initialization code goes here 
+    }
+
+    /**
+     * Checks access.
+     *
+     * @return bool
+     */
+    public function checkAccess()
+    {
+        $user = \Yii::$app->user->identity;
+
+        if (method_exists($user, 'getIsAdmin')) {
+            return $user->getIsAdmin();
+        } else if ($this->adminPermission) {
+            return $this->adminPermission ? \Yii::$app->user->can($this->adminPermission) : false;
+        } else {
+            return isset($user->username) ? in_array($user->username, $this->admins) : false;
+        }
     }
 }
