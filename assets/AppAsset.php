13c28ed7bebd6d308a8121e02651cf65b33aef1c
Jorge Gonzalez <ljorgelgonzalez@gmail.com>
2017-09-24 17:49:50 -0400

    test module

---
+++ assets/AppAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace lawiet\rbac\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * module for rbac.
+ *
+ * @author Jorge Gonzalez
+ * @email ljorgelgonzalez@outlook.com
+ *
+ * @since 1.0
+ */
+
+class RbacAsset extends AssetBundle
+{
+	
+}
