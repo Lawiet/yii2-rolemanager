f8e61f3377bb3dafb7bccea3480a92e8360c30c6
Jorge Gonzalez <ljorgelgonzalez@gmail.com>
2017-11-29 20:49:50 -0400

    remove multiselect and add selectivity

--- views/group/_form.php
+++ views/group/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use kartik\widgets\ActiveForm;
 use kartik\widgets\SwitchInput;
+use wbraganca\multiselect\MultiSelectWidget;
 
 /* @var $this yii\web\View */
 /* @var $model lawiet\rbac\models\Group */
@@ -25,6 +26,44 @@ use kartik\widgets\SwitchInput;
     <?= $form->field($model, 'status')->widget(SwitchInput::classname(), []); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'placeholder'=> Yii::t('app','Enter a Name...')]) ?>
+
+    <?= $form->field($model, 'date_created')->widget(MultiSelectWidget::classname(), [
+        'options' => [
+            'multiple' => 'multiple',
+        ],
+        'clientOptions' => [
+            'nonSelectedText' => Yii::t('app', 'Check an option!'),
+            'nSelectedText' => ' - ' . Yii::t('app', 'Options selected!'),
+            'allSelectedText' => Yii::t('app', 'All Selected'),
+            'selectAllText' => Yii::t('app', 'Select all'),
+            'numberDisplayed' => 1,
+            //'enableCaseInsensitiveFiltering' => true,
+            'maxHeight' => 600, // The maximum height of the dropdown. This is useful when using the plugin with plenty of options.
+            'includeSelectAllOption' => true
+        ],
+        'data' => ['Activo', 'inactivo'],
+        'model' => $model,
+        'attribute' => 'date_modified',
+    ]); ?>
+
+    <?= MultiSelectWidget::widget([
+        'options' => [
+            'multiple' => 'multiple',
+        ],
+        'clientOptions' => [
+            'nonSelectedText' => Yii::t('app', 'Check an option!'),
+            'nSelectedText' => ' - ' . Yii::t('app', 'Options selected!'),
+            'allSelectedText' => Yii::t('app', 'All Selected'),
+            'selectAllText' => Yii::t('app', 'Select all'),
+            'numberDisplayed' => 1,
+            //'enableCaseInsensitiveFiltering' => true,
+            'maxHeight' => 600, // The maximum height of the dropdown. This is useful when using the plugin with plenty of options.
+            'includeSelectAllOption' => true
+        ],
+        'data' => ['Activo', 'inactivo'],
+        'model' => $model,
+        'attribute' => 'date_modified',
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
